efa54cd8487125a20d633009cc11b6e3a118c29e
amit-singh88598 <amitsingh992728@gmail.com>
2025-03-22 20:29:21 +0530

    fixed the deployment issue

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../redux/userSlice";
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(registerUser(formData));
+    setFormData({ name: "", email: "", password: "" }); // Clear form
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Name"
+        required
+      />
+      <input
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+        required
+      />
+      <input
+        name="password"
+        type="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
